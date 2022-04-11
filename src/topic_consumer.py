61494ed2889f1207effeab421871f946cc4bce69
lgraef <lukas.graef@st.oth-regensburg.de>
2022-04-11 19:46:17 +0200

    Changes: - added content fetching and cleaning - added keyword extraction - added summaries if missing - prevent duplicate entries

--- src/topic_consumer.py
+++ src/topic_consumer.py
@@ -2,46 +2,95 @@ import faust
 import os
 import requests
 import logging
+import newspaper
+import traceback
+import pymongo
 
 from records import RssFeed
 from pymongo import MongoClient
 from bs4 import BeautifulSoup
+from newspaper import Article
+import nltk
+nltk.download('punkt')
 
 app = faust.App('topic_consumer', broker='kafka://localhost:9092')
-topic = app.topic('rss', value_type=RssFeed)
+rss = app.topic('rss', value_type=RssFeed)
+# rss_stream = app.stream(rss)
+
+rss_filtered = app.topic('rss_filtered', value_type=RssFeed)
 
 user = os.environ["MONGO_INITDB_ROOT_USERNAME"]
 pw = os.environ["MONGO_INITDB_ROOT_PASSWORD"]
 client = MongoClient("localhost", 27017, username=user, password=pw)
-db = client['test']
-collection = db['rss.content_test']
+db = client['data']
+collection = db['rss.articles']
+collection.create_index([('link', pymongo.TEXT)], name='link_index', unique=True)
+links = list(map(lambda link: link['link'], list(collection.find({}, {'link':1, '_id':0}))))
 
-def remove_old_articles(feed):
-    #TODO: fetch last update date from mongo and remove articles from stream that are older than that
-    pass
 
-async def fill_content_if_missing(feed):
-    if not feed.content:
-        try: 
-            link = feed.link
-            res = requests.get(link, timeout=3)
-            feed.content = res.text
-        except requests.exceptions.HTTPError as err:
+@app.timer(interval=36000.0)
+async def fetch_links():
+    links = list(map(lambda link: link['link'], list(collection.find({}, {'link':1, '_id':0}))))
+    
+@app.agent(rss) 
+async def remove_old_articles(feeds):
+    async for feed in feeds:
+        if not feed.link in links:
+            await rss_filtered.send(value=feed)
+
+async def fill_content(feed):
+    try:   
+        link = feed.link
+        article = Article(link)
+        article.download()
+        article.parse()
+        feed.content = article.text
+        if (not feed.content):
+            soup = BeautifulSoup(feed.content, features="html.parser")
+            # remove all script and style elements
+            for script in soup(["script", "style", "a", "img"]):
+                script.extract()
+            text = soup.get_text()
+            feed.content = text
+            article.download(input_html=text)
+        article.nlp()
+        feed.tags = article.keywords
+        feed.summary = article.summary
+    except newspaper.article.ArticleException as err:
             logging.warn(f"Could not fetch content from {link}")
-        except requests.exceptions.Timeout as err:
-            logging.warn(f"Timeout while fetching content from {link}")
     return feed    
-       
-def remove_html_tags(feed):
-    soup = BeautifulSoup(feed.content, "html.parser")
-    for data in soup(['style', 'script', 'head']):
-        data.decompose()
-        feed.content = ' '.join(soup.stripped_strings)
-    return feed
 
-s = app.stream(topic, processors = [fill_content_if_missing, remove_html_tags])
+async def fill_summary_if_missing(feed):
+    try:   
+        link = feed.link
+        article = Article("")
+        # clean existing summary from tags
+        if (feed.summary):
+            soup = BeautifulSoup(feed.summary, features="html.parser")
+            
+            # remove all script and style elements
+            for script in soup(["script", "style", "a", "img"]):
+                script.extract()
+
+            text = soup.get_text()
+            feed.summary = text
+        # summarize article if no summary exists
+        else: 
+            article.download(input_html=feed.content)
+            article.parse()
+            article.nlp()
+            feed.summary = article.summary
+    except Exception as err:
+            logging.warn(f"Could not summarize article: {link}")
+            traceback.print_exc()
+    return feed  
+
+s = app.stream(rss_filtered, processors = [fill_content, fill_summary_if_missing])
 
 @app.task       
 async def write_feed_to_mongo():
     async for feed in s:
-        collection.insert_one(feed.asdict())
+        try:
+            collection.insert_one(feed.asdict())
+        except Exception:
+            continue
